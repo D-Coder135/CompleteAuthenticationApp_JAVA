bed8720e2e186f02015c9b98c2eba799ad7abf92
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-16 21:04:01 +0530

    initialize the object emailField

--- app/src/main/java/com/example/completeauthenticationapp_java/EmailLoginPage.java
+++ app/src/main/java/com/example/completeauthenticationapp_java/EmailLoginPage.java
@@ -19,5 +19,6 @@ public class EmailLoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email_login_page);
+        emailField = findViewById(R.id.editText);
     }
 }
