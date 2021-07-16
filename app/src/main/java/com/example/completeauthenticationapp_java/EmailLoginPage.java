c78efa5b4666cba2f4e75ed33b0e185a6b6ee71b
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-16 21:02:53 +0530

    define the objects for the Button class

--- app/src/main/java/com/example/completeauthenticationapp_java/EmailLoginPage.java
+++ app/src/main/java/com/example/completeauthenticationapp_java/EmailLoginPage.java
@@ -3,10 +3,12 @@ package com.example.completeauthenticationapp_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class EmailLoginPage extends AppCompatActivity {
     EditText emailField, passwordField;
+    Button loginButton, signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
