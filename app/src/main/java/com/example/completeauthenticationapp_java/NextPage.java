17314dfd18081cf99a48858a487250a0f0192bb9
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-16 23:30:18 +0530

    define the object for the EditText class

--- app/src/main/java/com/example/completeauthenticationapp_java/NextPage.java
+++ app/src/main/java/com/example/completeauthenticationapp_java/NextPage.java
@@ -3,8 +3,10 @@ package com.example.completeauthenticationapp_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class NextPage extends AppCompatActivity {
+    EditText OTPField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
