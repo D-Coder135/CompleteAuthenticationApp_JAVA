511ac846aa931f5c51546058179e707ad75e6924
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-16 23:30:47 +0530

    define the object for the Button class

--- app/src/main/java/com/example/completeauthenticationapp_java/NextPage.java
+++ app/src/main/java/com/example/completeauthenticationapp_java/NextPage.java
@@ -3,10 +3,12 @@ package com.example.completeauthenticationapp_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class NextPage extends AppCompatActivity {
     EditText OTPField;
+    Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
