5e6f5be056c5c2de69a92295965ed94029febb5b
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-16 22:30:23 +0530

    initialize the objects inside the class SignUpPage.java

--- app/src/main/java/com/example/completeauthenticationapp_java/SignUpPage.java
+++ app/src/main/java/com/example/completeauthenticationapp_java/SignUpPage.java
@@ -3,6 +3,7 @@ package com.example.completeauthenticationapp_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -19,5 +20,12 @@ public class SignUpPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
+        emailField = findViewById(R.id.editText3);
+        passwordField = findViewById(R.id.editText4);
+        passwordField.setText(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        loginButton = findViewById(R.id.button5);
+        signUpButton = findViewById(R.id.button6);
+        progressBar = findViewById(R.id.progressBar2);
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 }
