1d6ba30e9edda6df60a958747bd597e77bbd4379
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-16 23:15:11 +0530

    define the object for the Button class

--- app/src/main/java/com/example/completeauthenticationapp_java/OTPLoginPage.java
+++ app/src/main/java/com/example/completeauthenticationapp_java/OTPLoginPage.java
@@ -3,12 +3,13 @@ package com.example.completeauthenticationapp_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.hbb20.CountryCodePicker;
 
 public class OTPLoginPage extends AppCompatActivity {
     CountryCodePicker countryCodePicker;
-
+    Button sendOTPButton, backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
