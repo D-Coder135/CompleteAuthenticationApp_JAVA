ecbb83bfb7946fd35f02a35cb015acecd6e889e2
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-16 22:34:10 +0530

    call the setOnClickListener() method along with the object signUpButton

--- app/src/main/java/com/example/completeauthenticationapp_java/SignUpPage.java
+++ app/src/main/java/com/example/completeauthenticationapp_java/SignUpPage.java
@@ -1,5 +1,6 @@
 package com.example.completeauthenticationapp_java;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,7 +10,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpPage extends AppCompatActivity {
@@ -36,6 +41,36 @@ public class SignUpPage extends AppCompatActivity {
                 Intent intent = new Intent(SignUpPage.this, EmailLoginPage.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailField.getText().toString();
+                String password = passwordField.getText().toString();
+
+                if (email.isEmpty()) {
+                    emailField.setError("Please Enter Your Email!");
+                } else {
+                    if (password.isEmpty()) {
+                        passwordField.setError("Please Enter Your Password!");
+                    }
+                }
+                progressBar.setVisibility(View.VISIBLE);
+                firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull @org.jetbrains.annotations.NotNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(SignUpPage.this, "Database Updated! Please Log In To Continue.", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(SignUpPage.this, EmailLoginPage.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText(SignUpPage.this, "Error! Could not update the Database.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
     }
