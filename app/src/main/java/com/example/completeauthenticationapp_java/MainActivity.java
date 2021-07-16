ed00fe860d00521cbc7cec840f97fcbd0d113030
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-16 20:56:11 +0530

    initialize the object for the Intent class

--- app/src/main/java/com/example/completeauthenticationapp_java/MainActivity.java
+++ app/src/main/java/com/example/completeauthenticationapp_java/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.completeauthenticationapp_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         emailLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this, EmailLoginPage.class);
             }
         });
     }
