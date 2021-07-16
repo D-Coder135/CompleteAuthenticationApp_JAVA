456981835b04de71dcf35c55066fdd374a1b8506
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-16 20:55:17 +0530

    initialize the object OTPLoginButton

--- app/src/main/java/com/example/completeauthenticationapp_java/MainActivity.java
+++ app/src/main/java/com/example/completeauthenticationapp_java/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         emailLoginButton = findViewById(R.id.button);
+        OTPLoginButton = findViewById(R.id.button2);
     }
 }
