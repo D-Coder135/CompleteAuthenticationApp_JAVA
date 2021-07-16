ac40ba4f68fac26911d0ee706c36acdafd4ebfbd
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-16 23:53:35 +0530

    call the startActivity() method

--- app/src/main/java/com/example/completeauthenticationapp_java/NextPage.java
+++ app/src/main/java/com/example/completeauthenticationapp_java/NextPage.java
@@ -94,6 +94,7 @@ public class NextPage extends AppCompatActivity {
                 } else {
                     Toast.makeText(NextPage.this, "Error! Database Not Updated.", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(NextPage.this, MainActivity.class);
+                    startActivity(intent);
 
                 }
             }
