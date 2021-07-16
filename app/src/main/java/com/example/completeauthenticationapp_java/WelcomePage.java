4e2dd374129a81f8afda84786881126db1a9e75d
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-17 00:02:56 +0530

    call the Toast.makeText() method

--- app/src/main/java/com/example/completeauthenticationapp_java/WelcomePage.java
+++ app/src/main/java/com/example/completeauthenticationapp_java/WelcomePage.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -24,6 +25,7 @@ public class WelcomePage extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 firebaseAuth.signOut();
+                Toast.makeText(WelcomePage.this, "Logged Out Successfully!", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(WelcomePage.this, MainActivity.class);
                 startActivity(intent);
                 finish();
